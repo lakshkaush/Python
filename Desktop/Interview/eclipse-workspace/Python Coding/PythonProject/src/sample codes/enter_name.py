b0ec242ce4b033f071a37ea60a3cf26bf505d001
Lakshitha Kaushik <Lakshitha@Lakshithas-MacBook-Pro.local>
2019-03-05 22:41:03 -0800

    Deleting unwanted dir

--- Desktop/Interview/eclipse-workspace/Python Coding/PythonProject/src/sample codes/enter_name.py
+++
@@ -1,5 +0,0 @@
-""" Enter name from prompt and display"""
-
-name = raw_input("What is your name?")
-print "Your name is:", name
-
--- Desktop/Interview/eclipse-workspace/Python Coding/PythonProject/src/sample codes/hello_world.py
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env python
-""" Sample hello world code"""
-print "My first hello world code"
